2171e6f25a7e791cddb7d1f47f69b14c05e752c5
Kylir Horton <kylirh@gmail.com>
2013-06-30 23:27:59 -0600

    Added some preliminary code for a drag 'n drop example that I might finish. Some day.

---
+++ code/dragon/dragon.js
@@ -0,0 +1,34 @@
+function setup() {
+     var dropzone = document.querySelectorAll("#drop-zone");
+}
+
+function handleDragStart(e) {
+     this.style.opacity = '0.4';  // this / e.target is the source node.
+}
+
+function handleDragOver(e) {
+     if (e.preventDefault) {
+          e.preventDefault(); // Necessary. Allows us to drop.
+     }
+
+     e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+
+     return false;
+}
+
+function handleDragEnter(e) {
+     // this / e.target is the current hover target.
+     this.addClassName('over');
+}
+
+function handleDragLeave(e) {
+     this.removeClassName('over');  // this / e.target is previous target element.
+}
+
+var cols = document.querySelectorAll('#columns .column');
+[].forEach.call(cols, function(col) {
+     col.addEventListener('dragstart', handleDragStart, false);
+     col.addEventListener('dragenter', handleDragEnter, false);
+     col.addEventListener('dragover', handleDragOver, false);
+     col.addEventListener('dragleave', handleDragLeave, false);
+});
